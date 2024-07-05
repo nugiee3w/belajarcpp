8a0cadb048549973f10e229f4f0596c39b564711
nugiee3w <nugielimit@gmail.com>
2024-07-05 08:09:12 +0800

    input

--- belajar01.cpp
+++ belajar01.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 int main(){
     std::cout <<"Masih belajar cpp" <<std::endl;
-    return 0;
+    
+using namespace std;
+int x; 
+cout << "Masukkan angka: "; // Type a number and press enter
+cin >> x; // Get user input from the keyboard
+cout << "Angka yang dimasukkan: " << x; // Display the input value
 }
